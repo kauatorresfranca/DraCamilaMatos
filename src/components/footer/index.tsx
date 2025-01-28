63e2d283461d798474413e319e78e29fc814f02d
kauatorresfranca <kauatorresfranca2@gmail.com>
2025-01-28 20:44:56 -0300

    adding the favicon

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,11 +1,12 @@
 import * as S from './styles'
 
 const Footer = () => {
-
+  const ano = new Date().getFullYear()
+  console.log(ano)
   return (
     <S.Background>
       <S.Container>
-        <p>© 2025 - Camila Matos - developed by Kauã Torres</p>
+        <p>© {2025} - Camila Matos - developed by Kauã Torres</p>
       </S.Container>
     </S.Background>
   )
